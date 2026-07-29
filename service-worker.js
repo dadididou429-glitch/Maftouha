5bd9563e3dd5a87f06afdc9d0346b83161bca225
dadididou429-glitch <dadididou429@gmail.com>
2026-07-29 03:24:00 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pharis-v8";
+const CACHE_NAME = "pharis-v10";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
